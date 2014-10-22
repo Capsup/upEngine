b7748335fd3fff906af29c41bca92b15b9677fa7
Capsup <capsup@live.dk>
2014-10-22 14:09:37 +0200

    IShader and GLSLShader

---
+++ upEngine/GLSLShader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upEngine
+{
+    /*public class GLSLShader : IShader
+    {
+        private int _vertexID;
+        private int _fragmentID;
+        private int _geometryID;
+
+        public int ID
+        {
+            get;
+            protected set;
+        }
+
+        public ShaderError AttachSource( string source, ShaderType shaderType )
+        {
+            switch( shaderType )
+            {
+                case ShaderType.VertexShader:
+                    _vertexID = GL.CreateShader();
+                    GL.ShaderSource( _vertexID, source );
+                    GL.CompileShader( _vertexID );
+                break;
+                case ShaderType.FragmentShader:
+                    _fragmentID = GL.CreateShader();
+                    GL.ShaderSource( _fragmentID, source );
+                    GL.CompileShader( _fragmentID );
+                break;
+                case ShaderType.GeometryShader:
+                    _geometryID = GL.CreateShader();
+                    GL.ShaderSource( _geometryID, source );
+                    GL.CompileShader( _geometryID );
+                break;
+                default:
+                    throw new ArgumentOutOfRangeException( "Unknown ShaderType" );
+            }
+        }
+
+        public ShaderError CompileShader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShaderError LinkShader()
+        {
+            throw new NotImplementedException();
+        }
+    }*/
+}
---
+++ upEngine/IShader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upEngine
+{
+    public enum ShaderError
+    {
+        NoError = -1,
+        CreationError,
+        SourceError,
+        CompileError,
+        LinkError
+    }
+
+    /*public enum ShaderType
+    {
+        VertexShader,
+        FragmentShader,
+        GeometryShader
+    }*/
+
+    public interface IShader
+    {
+        int ID
+        {
+            get;
+        }
+
+        //ShaderError AttachSource( string source, ShaderType shaderType );
+        ShaderError CompileShader();
+        ShaderError LinkShader();
+    }
+}
